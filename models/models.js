88e4227e7219ee043d23c07de8b068037d977d80
Miguel Angel <miguelangel.aparicio@gmail.com>
2015-07-20 03:21:23 +0200

    despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,12 +1,35 @@
 
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6] || null);
+var user     = (url[2] || null);
+var pwd      = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect  = (url[1] || null);
+var port     = (url[5] || null);
+var host     = (url[4] || null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require("sequelize");
 
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect: protocol,
+    protocol: protocol,
+    port: port,
+    host: host,
+    storage: storage, // solo SQLite (.env)
+    omitNull: true    // solo Postgres
+  }  
+);
+
 // Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,
-                {dialect: "sqlite", storage: "quiz.sqlite"});
+// var sequelize = new Sequelize(null, null, null,
+//                 {dialect: "sqlite", storage: "quiz.sqlite"});
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, "quiz"));
@@ -15,13 +38,13 @@ var Quiz = sequelize.import(path.join(__dirname, "quiz"));
 exports.Quiz = Quiz;
 
 // sequelize.sync() crea e iniciliza tabla de preguntas en DB
-sequelize.sync().sucess(function() {
+sequelize.sync().then(function() {
     // success(..) ejecuta el manejador una vez creada la tabla    
-    Quiz.count().sucess(function(count){
+    Quiz.count().then(function(count){
         if(count === 0) {  // la tabla se inicializa solo si esta vacia
             Quiz.create({ pregunta: "¿Cúal es la capital de Grecia?",
                           respuesta: "Atenas"})
-            .sucess(function(){console.log("Base de datos inicializada")});
+            .then(function(){console.log("Base de datos inicializada")});
         };
     });
 });
